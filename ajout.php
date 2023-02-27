8de4d1dd82a694ccbb8249057fa9530d96f13f67
Mathbech <legeekyou@gmailcom>
2023-02-27 18:27:00 +0100

    Version statique du site

---
+++ ajout.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ajout stock</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="assets/images/favicon.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+    <?php
+        include_once('partials/_navbar.php');
+      ?>
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <!-- Inclure sidebar avec php -->
+        <?php 
+        include_once('partials/_sidebar.php');
+        ?>
+        
+        <div class="col-md-6 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Ajout de stock</h4>
+                    <p class="card-description"> Veuillez remplir le formulaire pour ajouter du stock </p>
+                    <form class="forms-sample">
+                      <div class="form-group row">
+                        <label for="type_de_filament" class="col-sm-3 col-form-label">Type de filament</label>
+                        <div class="col-sm-9">
+                          <!-- <input type="text" class="form-control" id="exampleInputUsername2" placeholder="Username"> -->
+                          <select name="type" class="typeFilament" id="type_de_filament">
+                          <option value="">Sélectionnez le type de filament</option>
+                            <option value="PLA">PLA</option>
+                            <option value="PETG">PETG</option>
+                            <option value="ABS">ABS</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="nombre_bobine" class="col-sm-3 col-form-label">Nombre de bobine</label>
+                        <div class="col-sm-9">
+                          <input type="number" class="qte" id="nombre_bobine" placeholder="Nombre de bobine">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="poids" class="col-sm-3 col-form-label">Poids total</label>
+                        <div class="col-sm-9">
+                          <input type="text" class="poids" id="poids" placeholder="Poids total">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="couleur_filament" class="col-sm-3 col-form-label">couleur</label>
+                        <div class="col-sm-9">
+                          <input type="text" class="couleur" id="couleur_filament" placeholder="couleur">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="prix" class="col-sm-3 col-form-label">prix total payé</label>
+                        <div class="col-sm-9">
+                          <input type="text" class="form-control" id="prix" placeholder="prix total payé">
+                        </div>
+                      </div>
+                      <button type="submit" class="btn btn-primary mr-2">Submit</button>
+                      <button class="btn btn-light">Cancel</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+        </div>
+        
+        <!-- content-wrapper ends -->
+          <!-- partial:../../partials/_footer.html -->
+          <?php
+            include_once('./partials/_footer.php');
+          ?>
+          <!-- partial -->
+        <!-- main-panel ends -->
+      <!-- page-body-wrapper ends -->
+    </div>
+
+    
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="./assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="./assets/js/off-canvas.js"></script>
+    <script src="./assets/js/hoverable-collapse.js"></script>
+    <script src="./assets/js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <!-- End custom js for this page -->
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Inscription</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="./assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="./assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="./assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="./assets/images/favicon.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth">
+          <div class="row flex-grow">
+            <div class="col-lg-4 mx-auto">
+              <div class="auth-form-light text-left p-5">
+                <div class="brand-logo">
+                  <img src="./assets/images/logo-dark.svg">
+                </div>
+                <h4>Tu es nouveau?</h4>
+                <h6 class="font-weight-light">inscrit toi, c'est gratuit et rapide. En quelques étapes c'est fait</h6>
+                <form class="pt-3">
+                  <div class="form-group">
+                    <input type="text" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Nom d'utilisateur">
+                  </div>
+                  <div class="form-group">
+                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
+                  </div>
+                  <!-- <div class="form-group">
+                    <select class="form-control form-control-lg" id="exampleFormControlSelect2">
+                      <option>Country</option>
+                      <option>United States of America</option>
+                      <option>United Kingdom</option>
+                      <option>India</option>
+                      <option>Germany</option>
+                      <option>Argentina</option>
+                    </select>
+                  </div> -->
+                  <div class="form-group">
+                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Mot de passe">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword2" placeholder=" Confirmer le Mot de passe">
+                  </div>
+                  <div class="mb-4">
+                    <div class="form-check">
+                      <label class="form-check-label text-muted">
+                        <input type="checkbox" class="form-check-input"> J'accepte les termes et les conditions </label>
+                    </div>
+                  </div>
+                  <div class="mt-3">
+                    <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="./dashboard.php">S'INSCRIRE</a>
+                  </div>
+                  <div class="text-center mt-4 font-weight-light"> Tu as déjà un compte? <a href="index.php" class="text-primary">connecte toi!</a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="./assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="./assets/js/off-canvas.js"></script>
+    <script src="./assets/js/hoverable-collapse.js"></script>
+    <script src="./assets/js/misc.js"></script>
+    <!-- endinject -->
+  </body>
+</html>
