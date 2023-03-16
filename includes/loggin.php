ad6e20e4b2492c30088acab13435201a3a5252e4
Mathbech <legeek.you@gmail.com>
2023-03-16 19:12:34 +0100

    Vérification si le compte est activé ou non

--- includes/loggin.php
+++ includes/loggin.php
@@ -7,25 +7,35 @@ if (!empty($_POST['loggin']) && !empty($_POST['password'])) {
 
 	//var_dump($username);
 	//var_dump($password);
-
-	$q = $conn->prepare("SELECT id, psw FROM `users` WHERE `loggin` = :loggin");
+//Préparation requête pour récupérer les id de l'utilisateur
+	$q = $conn->prepare("SELECT id, psw, active FROM `users` WHERE `loggin` = :loggin");
 
 	$q->bindValue('loggin', $username);
+	//exécution de la requête
 	$q->execute();
 	$result = $q->fetch(PDO::FETCH_ASSOC);
 
-	var_dump($result['id']);
-	
+	// var_dump($result['id']);
+
+
+	//Si résultat obtenu continuer
 	if ($result) {
 		$id = $result['id'];
+		$active = $result['active'];
 		$passwordhash = $result['psw'];
+		//Vérification du mot de passe
 		if(password_verify($password, $passwordhash)){
-			$_SESSION['connect'] = true;
-			$_SESSION['username']= $username;
-			$_SESSION['id'] = $id;
-			echo "Connexion réussie ";
-			echo "id = ", $result['id'];
-			header('location: ../dashboard.php');
+			//vérification du compte (actif ou non)
+			if($active == 1){
+				$_SESSION['connect'] = true;
+				$_SESSION['username']= $username;
+				$_SESSION['id'] = $id;
+				echo "Connexion réussie ";
+				echo "id = ", $result['id'];
+				header('location: ../dashboard.php');
+			}else{
+				echo("Votre compte à été désactivé");
+			}
 		}else {
 			echo "Identifiant invalides ou innexistant";
 		}
