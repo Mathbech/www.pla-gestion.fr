d146c2ab750aae069d7402b480dfe681553e2f73
Bébech <legeek.you@gmail.com>
2023-05-24 18:15:00 +0200

    Création d'une page de gestion des utilisateurs, et finalisation de la page settings.php

---
+++ admin/users.php
@@ -0,0 +1,87 @@
+<?php
+    session_start();
+    $username = $_SESSION['username'];
+    $id = $_SESSION['id'];
+
+    require_once('../includes/log.php');
+    isloggedin();
+
+    $timezone = date_default_timezone_get();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Dashboard</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="../assets/images/favicon.png" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- inclure la nav bar php -->
+        <?php        
+        include_once('./includes/_navbar.php');
+        nav($username, $id);
+        ?>
+        <div class="container-fluid page-body-wrapper">
+            <!-- Inclure sidebar avec php -->
+            <?php
+            include_once('./includes/_sidebar.php');
+            side($username, $id);
+            ?>
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="d-xl-flex justify-content-between align-items-start">
+                        <h2 class="text-dark font-weight-bold mb-2"> Utilisateurs </h2>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <?php
+        include_once('../includes/_footer.php');
+        footer();
+        ?>
+    </div>
+    <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="../assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="../assets/vendors/jquery-circle-progress/js/circle-progress.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="../assets/js/off-canvas.js"></script>
+    <script src="../assets/js/hoverable-collapse.js"></script>
+    <script src="../assets/js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="../assets/js/dashboard.js"></script>
+    <!-- End custom js for this page -->
+</body>
+
+</html>
--- settings.php
+++ settings.php
@@ -9,67 +9,91 @@ isloggedin();
 
 require_once('./includes/connect.php');
 if (isset($_POST)) {
-    $q = $conn->prepare("SELECT mail FROM `users` WHERE `loggin` = :loggin");
+    $q = $conn->prepare("SELECT mail, psw FROM `users` WHERE `loggin` = :loggin");
     //Préparation des valeurs
     $q->bindValue(':loggin', $username);
     //exécution de la requête
     $q->execute();
     $data = $q->fetch(PDO::FETCH_ASSOC);
+    $passwordhash = $data['psw'];
 }
 
 
 if (isset($_POST['email'])) {
-    if ($_POST['email'] === $data['mail']) {
-        if (isset($_POST['password']) && isset($_POST['cpassword'])) {
-            $err = "";
-            $valid = "";
-            // var_dump($_POST);
-            $mail = $_POST['email'];
-            $pass = $_POST['password'];
-            $cpassword = $_POST['cpassword'];
+    if (isset($_POST['apassword'])) {
+        $apassword = $_POST['apassword'];
+        $err = "";
+        $valid = "";
+        if (password_verify($apassword, $passwordhash)) {
+            // if($apassword === $passwordhash) {
+            if ($_POST['email'] === $data['mail']) {
+                if (isset($_POST['password']) && isset($_POST['cpassword'])) {
+                    // var_dump($_POST);
+                    $mail = $_POST['email'];
+                    $pass = $_POST['password'];
+                    $cpassword = $_POST['cpassword'];
 
-            if ($pass == $cpassword) {
-                $password = password_hash($pass, PASSWORD_DEFAULT);
-                // var_dump($password);
-                $r = $conn->prepare("UPDATE users SET psw = :pswd WHERE id = :id");
-                // $r->bindValue(':email', $mail);
-                $r->bindValue(':pswd', $password);
-                $r->bindValue(':id', $id);
-                $result = $r->execute();
+                    if ($pass == $cpassword) {
+                        $password = password_hash($pass, PASSWORD_DEFAULT);
+                        // var_dump($password);
+                        $r = $conn->prepare("UPDATE users SET psw = :pswd WHERE id = :id");
+                        // $r->bindValue(':email', $mail);
+                        $r->bindValue(':pswd', $password);
+                        $r->bindValue(':id', $id);
+                        $result = $r->execute();
 
-                if ($result) {
-                    $valid = 'Mot de passe modifié avec succès';
+                        if ($result) {
+                            $valid = 'Mot de passe modifié avec succès';
+                        }else{
+                            $err = 'Une erreur est survenue';
+                        }
+
+                    } else {
+                        $err = 'Les mots de passes ne correspondent pas!';
+                    }
+                } else {
+                    $err = 'Merci de remplir les champs de mot de passe';
                 }
-
             } else {
-                $err = 'Les mots de passes ne correspondent pas!';
+                // if (isset($_POST['password']) && isset($_POST['cpassword'])) {
+                //     $mail1 = $_POST['email'];
+                //     $pass1 = $_POST['password'];
+                //     $cpassword1 = $_POST['cpassword'];
+                //     if ($pass1 == $cpassword1) {
+                //         $password1 = password_hash($pass1, PASSWORD_DEFAULT);
+
+                //         $r1 = $conn->prepare("UPDATE users SET mail = :email, psw = :pswd WHERE id = :id");
+                //         $r1->bindValue(':email', $mail1);
+                //         $r1->bindValue(':pswd', $password1);
+                //         $r1->bindValue(':id', $id);
+                //         $result1 = $r1->execute();
+
+                //         if ($result1) {
+                //             $valid = 'Informations modifiés avec succès';
+                //         }
+                //     } else {
+                //         $err = 'Les mots de passes ne correspondent pas!';
+                //     }
+                // }else{
+                    $mail2 = $_POST['email'];
+                    $r2 = $conn->prepare("UPDATE users SET mail = :email WHERE id = :id");
+                    $r2->bindValue(':email', $mail2);
+                    $r2->bindValue(':id', $id);
+                    $result2 = $r2->execute();
+
+                    if ($result2) {
+                        $valid = 'Email modifié avec succès';
+                    }
+                // }
             }
         } else {
-            $err = 'Merci de remplir les champs de mot de passe';
+            $err = 'L\'ancien mot de passe saisie n\'est pas correct';
         }
-    } else {
-        if (isset($_POST['password']) && isset($_POST['cpassword'])) {
-            $mail1 = $_POST['email'];
-            $pass1 = $_POST['password'];
-            $cpassword1 = $_POST['cpassword'];
-            if ($pass1 == $cpassword1) {
-                $password1 = password_hash($pass1, PASSWORD_DEFAULT);
-
-                $r1 = $conn->prepare("UPDATE users SET mail = :email, psw = :pswd WHERE id = :id");
-                $r1->bindValue(':email', $mail1);
-                $r1->bindValue(':pswd', $password1);
-                $r1->bindValue(':id', $id);
-                $result1 = $r1->execute();
-
-                if ($result1) {
-                    $valid = 'Informations modifiés avec succès';
-                }
-            } else {
-                $err = 'Les mots de passes ne correspondent pas!';
-            }
-        }
+    }else{
+        $err = 'Merci de saisir votre ancien mot de passe';
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -126,6 +150,11 @@ if (isset($_POST['email'])) {
                                 <label for="exampleInputPassword4">Confirmez le mot de passe</label>
                                 <input type="password" name="cpassword" class="form-control" id="exampleInputPassword4"
                                     placeholder="Confirmez le mot de passe">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword4">Entrez votre mot de passe</label>
+                                <input type="password" name="apassword" class="form-control" id="exampleInputPassword4"
+                                    placeholder="Entrez votre mot de passe">
                             </div>
                             <p class="text-danger">
                                 <?php if (isset($err))
